0fa6ac927a3ab7bfe71d1198d8941ba1c8eea680
hari328 <hari328@ymail.com>
2018-09-30 20:13:14 +0530

    background process

--- run.sh
+++ run.sh
@@ -4,7 +4,12 @@ sh setup.sh
 source activate venv-jvcr
 
 # no args behaviour of training.py is to download dataset.
-python ./src/training.py -mode=pre-train --pre_train_mode=hourglass --optimizer=SGD --momentum=0.1
+python ./src/training.py -mode=pre-train --pre_train_mode=hourglass --optimizer=SGD --momentum=0.1 &
+
+backgroundProcess=$!
+
+# use the process id stored in backgroundProcess to stop the process.
+echo "bacg process is $backgroundProcess"
 
 # deactivate virtual environment 
 source deactivate venv-jvcr
